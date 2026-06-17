6875753ba02fcad0be9cee0ad7e9a6bca7f547c2
Zo <zo@zo.computer>
2026-06-17 18:41:38 +0000

    iOS: fix OpenAL ALCdevice/ALCcontext typedef clash on Apple in SoundSystemAL

--- minecraftpe/headers/sound/SoundSystemAL.hpp
+++ minecraftpe/headers/sound/SoundSystemAL.hpp
@@ -2,13 +2,24 @@
 #if (defined(__linux__) || defined(__APPLE__)) && !defined(ANDROID)
 #include <_types.h>
 #include <sound/SoundSystem.hpp>
+#ifdef __APPLE__
+// On Apple, ALCdevice/ALCcontext are typedefs (typedef struct ALCdevice_struct
+// ALCdevice), so `struct ALCdevice*` would create a conflicting tag. Pull in
+// the real OpenAL headers and use the typedef names directly.
+#include <OpenAL/alc.h>
+#endif
 
 struct SoundSystemAL: public SoundSystem
 {
 	static const int MAX_PLAYED = 4;
 
+#ifdef __APPLE__
+	ALCdevice* device = 0;
+	ALCcontext* context = 0;
+#else
 	struct ALCdevice* device = 0;
 	struct ALCcontext* context = 0;
+#endif
 	int playedCnt = 0;
 	uint32_t buffers[MAX_PLAYED] = {0};
 	uint32_t sources[MAX_PLAYED] = {0};
